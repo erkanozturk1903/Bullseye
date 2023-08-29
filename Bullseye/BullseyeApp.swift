25af5e1e2628a9f2ed19aeda591f0e93f2c84ff5
erkanozturk1903 <erkanozturk1903@yahoo.com.tr>
2023-08-29 10:20:43 +0300

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Erkan Ozturk on 29.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
