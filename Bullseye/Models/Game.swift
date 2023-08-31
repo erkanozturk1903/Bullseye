111d174721a5d412dd6d0127234bff20ea0e78d7
erkanozturk1903 <erkanozturk1903@yahoo.com.tr>
2023-08-31 18:26:53 +0300

    V1

---
+++ Bullseye/Models/Game.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+struct Game{
+    var target: Int = 37
+    var sccore: Int = 0
+    var round: Int = 1
+
+    func points(sliderValue: Int) -> Int {
+        return 999
+    }
+    
+}
